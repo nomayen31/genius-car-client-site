ac3e8f7adebcdf01f2e63db092a8ab2b508b27ae
nomayen <nomayen.ohin@gmail.com>
2023-01-10 20:11:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    
+
+    return (
+        <div data-theme="light" className='max-w-screen-xl mx-auto'>
+           <RouterProvider router={router}></RouterProvider>
+        </div>
+    );
+};
 
 export default App;
