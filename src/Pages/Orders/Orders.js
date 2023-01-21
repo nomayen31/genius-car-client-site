357d81f0a9ab7f6bb3410e9048570873247e4c8d
nomayen <nomayen.ohin@gmail.com>
2023-01-21 11:51:05 +0600

    orders data loded

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Orders = () => {
     const {user} =useContext(AuthContext);
+    const[orders, setOrders]=useState({})
+
+
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/orders?email=${user?.email}`)
+        .then(res =>res.json())
+        .then(data =>setOrders(data));
+    }, [user?.email])
+
+
+
 
     return (
         <div>
-            
+            <h2 className='text-5xl'>You Have {orders.length} orders</h2>
         </div>
     );
 };
